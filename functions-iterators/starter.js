6deffd516b91748ef91bfc2006c64630db35b40a
arfajraa <arfajraa@gmail.com>
2018-11-15 01:36:14 +0300

    added completed part 7

--- functions-iterators/starter.js
+++ functions-iterators/starter.js
@@ -29,28 +29,28 @@ This will help us track your progress and it will give you practice writing good
 
 // PART 0: Just DO it!!
 
-var justDoIt = function(str){
+var justDoIt = function (str) {
   return `${str}, JUST DO IT!`;
 }
 
 // PART 1: Big or Small String?
 
-var bigOrSmallString = function(str){
-  if(str.length > 10){
+var bigOrSmallString = function (str) {
+  if (str.length > 10) {
     return "This word is loooooong!";
   }
-  else{
+  else {
     return "This word is short 😬";
   }
 }
 
 // PART 2: Odd or Even String Length?
 
-var oddOrEvenString = function(str){
-  if(str.length % 2 === 0){
+var oddOrEvenString = function (str) {
+  if (str.length % 2 === 0) {
     return "This string length is TOTALLY even!";
   }
-  else{
+  else {
     return "This string's length is odd..";
   }
 
@@ -58,26 +58,26 @@ var oddOrEvenString = function(str){
 
 // PART 3: Median
 
-var testArray = [1,2,8,4,5,6,7,3,9,10];
+var testArray = [1, 2, 8, 4, 5, 6, 7, 3, 9, 10];
 
-var medianOfArray = function(arr){
-  arr.sort(function(a, b){return a - b});
+var medianOfArray = function (arr) {
+  arr.sort(function (a, b) { return a - b });
   var midpoint = Math.floor(arr.length / 2);
-  if ((arr.length % 2) !== 0){
+  if ((arr.length % 2) !== 0) {
     return /* `${arr} >>> ` + */ arr[midpoint];
   }
   else {
     //median of array with even number of elements, avg of the two middle elements.
     return /* `${arr} >>> ` + */ ((arr[midpoint] + arr[midpoint - 1]) / 2);
   }
-  
+
 }
 
 // PART 4: Sum Array
 
-var sumArray = function (arr){
+var sumArray = function (arr) {
   var sum = 0;
-  for (var i = 0; i < arr.length; i ++){
+  for (var i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
   return sum;
@@ -87,13 +87,13 @@ var sumArray = function (arr){
 
 var testString = "Neil Patrick Harris";
 
-var vowelCount = function (str){
+var vowelCount = function (str) {
   var sum = 0;
   var chr = "";
   str = str.toUpperCase();
-  for (var i = 0; i < str.length; i++){
+  for (var i = 0; i < str.length; i++) {
     chr = str.charAt(i);
-    if (chr == "A" || chr == "E" || chr == "I" || chr == "O" || chr == "U"){
+    if (chr == "A" || chr == "E" || chr == "I" || chr == "O" || chr == "U") {
       sum += 1;
     }
   }
@@ -103,12 +103,12 @@ var vowelCount = function (str){
 
 // PART 6: Initials
 
-var initials = function (str){
+var initials = function (str) {
 
   var result = str.charAt(0);
 
-  for (var i = 0; i < str.length; i++){
-    if (str.charAt(i) == " "){
+  for (var i = 0; i < str.length; i++) {
+    if (str.charAt(i) == " ") {
       result += str.charAt(i + 1);
     }
   }
@@ -117,7 +117,29 @@ var initials = function (str){
 
 // PART 7: Days of the week
 
+var daysOfWeek = function () {
+  var selectedDay = prompt("Please enter the day:")
 
+  switch (selectedDay) {
+    case "Monday":
+      alert("Energize!");
+      break;
+    case "Tuesday":
+      alert("Just getting started!");
+      break;
+    case "Wednesday":
+      alert("Hump Hump!");
+      break;
+    case "Thursday":
+      alert("Almost there!");
+      break;
+    case "Friday" || "Saturday" || "Sunday":
+      alert("Weeeeeee!");
+      break;
+    default:
+      alert("Huh, sorry didn't get that?");
+  }
+}
 
 // PART 8: Let's take the Subway
 
