bd78bca36291442ce4c642129601db356e4541c5
marwah14 <memo_224@live.com>
2018-11-18 09:06:54 +0300

    finished homework

--- functions-iterators/starter.js
+++ functions-iterators/starter.js
@@ -1,9 +1,9 @@
 /* ============ BEFORE YOU GET STARTED ============ */
 
 /* here is a very short function that pops up an alert window that says 'Hello!' */
-function hello() {
-  alert('Hello!');
-}
+//function hello() {
+  //alert('Hello!');
+//}
 
 /* Here's the steps to test it:
 
@@ -29,37 +29,93 @@ This will help us track your progress and it will give you practice writing good
 
 // PART 0: Just DO it!!
 
+  function justDoIt(name){
+var name = "Marwah";
+    return `${name}  just Do It`;
+  };
+   justDoIt('Marwah');
+
 
 
 // PART 1: Big or Small String?
+function bigOrsmallString(amazingDay){
+   if (bigOrsmallString.lenght > 10 ){ 
+return "This word is loooooong!";
+  } else {
+    return "This word is short 😬";
+  };
+}
+bigOrsmallString();
+
 
 
 
 // PART 2: Odd or Even String Length?
 
+function oddOrEvenString(argumt){
+if (oddOrEvenString.lenght % 2 ===0){
+  return "This string's length is odd";
+} else{
+  return "This string length is TOTALLY even!"
+};
+}
+oddOrEvenString();
+
+
 
 
 // PART 3: Median
+function medianOfArray(){
+
+};
 
 
 
 // PART 4: Sum Array
-
-
+var arr = [20, 30, 10, 14];
+function sumArray(arr){
+  if (arr == 0){
+    return 0;
+  }
+}
+arr.reduce(sumArray);
 
 // PART 5: Vowel Count
 
+function vowelCount(str){
+  
 
 
 // PART 6: Initials
-
+var name = "First name, Middel name";
+function Initials(name){
+return
+}
 
 
 // PART 7: Days of the week
+var days = prompt("What days is today?");
 
+if (days === Monday){
+    alert('Your message here');
+  } else if( days === Tuesday){
+alert("Just getting started");
+  } else if(days === Wendesday){
+    alert("Hump Hump");
+} else if(days === Thursday){
+alert("Almost there");
+} else if(days === Friday || Saturday || Sunday){
+  alert("Weeeee!");
+} else{
+  alert("Huh, sorry didn't get that?")
+}
+
+
+ 
 
 
 // PART 8: Let's take the Subway
+
 
 
 
